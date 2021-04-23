b9ceab0e4b71c7e49acc701c9d17dce412317bf3
cesare <cesare.magnetti98@gmail.com>
2021-04-23 12:39:45 +0100

    added more helper functions (frobenius norm and gatys et Al. content/ style extraction)

--- utils/helperFunctions.py
+++ utils/helperFunctions.py
@@ -1,9 +1,11 @@
+import torch
+import torch.nn as nn
 from torchvision.utils import make_grid
 import numpy as np
 from matplotlib import pyplot as plt
 
 def show(img):
-    npimg = img.cpu().numpy()
+    npimg = img.detach().cpu().numpy().squeeze()
     plt.imshow(np.transpose(npimg, (1,2,0)))
     plt.show()
 
@@ -15,3 +17,30 @@ def show_batch(tensors, nrow = 8, denorm = None):
     tensors = make_grid(tensors, nrow=nrow, padding=2, normalize=True,
                             range=None, scale_each=False, pad_value=0)
     show(tensors)
+
+def Gram(tensor):
+    return torch.mm(tensor, tensor.t())
+
+def get_content_style_representations_gatys(content, style):
+    # content representations is simply the vectorized features (single scale)
+    Cc, Hc, Wc = content.squeeze().shape
+    content_repr = content.view(Cc, -1)
+
+    # style will be the Gram matrix of each style activation (multi-scale)
+    style_reprs = []
+    for _style in style:
+        Cs, Hs, Ws = _style.squeeze().shape
+        style_repr = _style.view(Cc, -1)
+        style_reprs.append(Gram(style_repr))
+    
+    return content_repr, style_reprs
+
+class FrobeniusNorm(nn.Module):
+    """
+    simple class handle to use frobenius norm between two matrices in optimisation process
+    """
+    def __init__(self):
+        super(FrobeniusNorm, self).__init__()
+    
+    def forward(self, x1, x2):
+        return torch.linalg.norm((x1-x2), 'fro')
