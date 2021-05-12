3abcf60e0f5576c5557a1f1f90eb4c3b2e88ae18
Cesare Magnetti <cm1320@gpu11.doc.ic.ac.uk>
2021-05-12 17:32:39 +0100

    removed useless code

--- setup_repo.sh
+++
@@ -1,9 +0,0 @@
-# run this code on a clean folder to download CUT/CycleGAN repo and setup the working environment.
-git clone https://github.com/taesungp/contrastive-unpaired-translation CUT
-
-# create virtual environment with python3
-python3 -m venv env
-source env/bin/activate
-
-# install dependencies
-pip install -r CUT/requirements.txt
