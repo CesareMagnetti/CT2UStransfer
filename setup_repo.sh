8e19b493c59f3d7e4a056f6fe35c0304e182245d
Cesare Magnetti <cm1320@gpu13.doc.ic.ac.uk>
2021-05-12 15:19:42 +0100

    adding setup script to clone CUT repo and setup work environment

--- setup_repo.sh
+++ setup_repo.sh
@@ -1,6 +1,5 @@
 # run this code on a clean folder to download CUT/CycleGAN repo and setup the working environment.
 git clone https://github.com/taesungp/contrastive-unpaired-translation CUT
-cd CUT
 
 # create virtual environment with python3
 python3 -m venv env
