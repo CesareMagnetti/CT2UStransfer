618af7189627a44952590b5b4d15bcd2f111e209
Cesare Magnetti <cm1320@gpu09.doc.ic.ac.uk>
2021-05-19 11:59:16 +0100

    small updates

--- test_CycleGAN.sh
+++ test_CycleGAN.sh
@@ -4,4 +4,4 @@
 mkdir -p /vol/bitbucket/cm1320/CycleGAN/test/
 
 # launch experiment
-python CycleGAN/test.py --dataroot /vol/biomedic3/hjr119/US_GEN_W_CUT/CUT/datasets/ct2us_hp --checkpoints_dir /vol/bitbucket/cm1320/CycleGAN/checkpoints/ --results_dir /vol/bitbucket/cm1320/CycleGAN/test/ --name experiment1 --model cycle_gan --input_nc 1 --output_nc 1
+python CycleGAN/test.py --dataroot /vol/biomedic3/hjr119/US_GEN_W_CUT/CUT/datasets/ct2us_hp --checkpoints_dir /vol/bitbucket/cm1320/CycleGAN/checkpoints/ --results_dir /vol/bitbucket/cm1320/CycleGAN/test/ --name LPIPScycle_noIdentity --model cycle_gan --input_nc 1 --output_nc 1 --epoch 190
--- train_CycleGAN.sh
+++ train_CycleGAN.sh
@@ -4,5 +4,5 @@
 mkdir -p /vol/bitbucket/cm1320/CycleGAN/checkpoints/
 
 # launch experiment
-python -m visdom.server # for visualisation
-python CycleGAN/train.py --dataroot /vol/biomedic3/hjr119/US_GEN_W_CUT/CUT/datasets/ct2us_hp --checkpoints_dir /vol/bitbucket/cm1320/CycleGAN/checkpoints/ --name LPIPScycle --model cycle_gan --input_nc 1 --output_nc 1 --batch_size 8 --cycle_loss LPIPS --n_epochs 200 --n_epochs_decay 200
+#python -m visdom.server # for visualisation
+python CycleGAN/train.py --dataroot /vol/biomedic3/hjr119/US_GEN_W_CUT/CUT/datasets/ct2us_hp --checkpoints_dir /vol/bitbucket/cm1320/CycleGAN/checkpoints/ --name LPIPScycle_noIdentity --model cycle_gan --input_nc 1 --output_nc 1 --batch_size 1 --cycle_loss LPIPS --save_epoch_freq 10 --n_epochs 100 --n_epochs_decay 100 --lambda_identity 0 --epoch 190
