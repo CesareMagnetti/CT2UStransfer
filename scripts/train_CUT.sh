1bf47869412d630217d88ba7ca5078f3b54494d7
Cesare Magnetti <cm1320@gpu11.doc.ic.ac.uk>
2021-05-23 19:28:32 +0100

    updating code

--- scripts/train_CUT.sh
+++ scripts/train_CUT.sh
@@ -5,4 +5,4 @@ mkdir -p /vol/bitbucket/cm1320/CUT/checkpoints/
 
 # launch experiment
 python -m visdom.server # for visualisation
-python CUT/train.py --dataroot /vol/biomedic3/hjr119/US_GEN_W_CUT/CUT/datasets/ct2us_hp --checkpoints_dir /vol/bitbucket/cm1320/CUT/checkpoints/ --name CT2US_CUTnoNCEtoLayer0 --model cut --CUT_mode CUT --nce_layers 4,8,6,12,16 --input_nc 1 --output_nc 1 --batch_size 4
+python CUT/train.py --dataroot /vol/biomedic3/hjr119/US_GEN_W_CUT/CUT/datasets/ct2us_hp --checkpoints_dir /vol/bitbucket/cm1320/CUT/checkpoints/ --name CT2US_CUTnoNCEtoLayer0 --model cut --CUT_mode CUT --nce_layers 4,8,6,12,16 --input_nc 1 --output_nc 1 --batch_size 2
