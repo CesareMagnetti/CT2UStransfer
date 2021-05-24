55c214a76562acb380f43e8428575d38b68c9b89
Cesare Magnetti <cm1320@gpu11.doc.ic.ac.uk>
2021-05-24 12:03:27 +0100

    updated code

--- launch_preliminary_experiments.sh
+++ launch_preliminary_experiments.sh
@@ -21,3 +21,10 @@ python CUT/train.py --dataroot /vol/biomedic3/hjr119/US_GEN_W_CUT/CUT/datasets/c
 # use LPIPS as a cycle consistency loss both for the model with and without the idt loss
 python CycleGAN/train.py --dataroot /vol/biomedic3/hjr119/US_GEN_W_CUT/CUT/datasets/ct2us_hp --checkpoints_dir /vol/bitbucket/cm1320/CT2US/checkpoints/ --name CycleGAN_LPIPS --model cycle_gan --input_nc 1 --output_nc 1 --batch_size 2 --cycle_loss LPIPS --save_epoch_freq 10
 python CycleGAN/train.py --dataroot /vol/biomedic3/hjr119/US_GEN_W_CUT/CUT/datasets/ct2us_hp --checkpoints_dir /vol/bitbucket/cm1320/CT2US/checkpoints/ --name CycleGAN_LPIPS_noIdtLoss --model cycle_gan --input_nc 1 --output_nc 1 --batch_size 2 --cycle_loss LPIPS --save_epoch_freq 10 --lambda_identity 0
+
+# ==== 4th experiment ====
+
+# CUT with standard configuration and removed layer 0 from PatchNCE loss
+python CUT/train.py --dataroot /vol/biomedic3/hjr119/US_GEN_W_CUT/CUT/datasets/ct2us_hp --checkpoints_dir /vol/bitbucket/cm1320/CT2US/checkpoints/ --name CUT_standard_noLayer0 --model cut --CUT_mode CUT --input_nc 1 --output_nc 1 --batch_size 2 --save_epoch_freq 10 --nce_layers 4,8,12,16
+# CUT without identity loss and removed layer 0 from PatchNCE loss
+python CUT/train.py --dataroot /vol/biomedic3/hjr119/US_GEN_W_CUT/CUT/datasets/ct2us_hp --checkpoints_dir /vol/bitbucket/cm1320/CT2US/checkpoints/ --name CUT_noIdtLoss_noLayer0 --model cut --CUT_mode FastCUT --input_nc 1 --output_nc 1 --batch_size 2 --save_epoch_freq 10 --nce_layers 4,8,12,16
